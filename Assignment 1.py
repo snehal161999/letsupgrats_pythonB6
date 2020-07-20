1b5ef08878c3d8276e9df88f96654360f8f09985
snehal161999 <68274013+snehal161999@users.noreply.github.com>
2020-07-20 13:15:23 +0530

    Create Assignment 1.py

---
+++ Assignment 1.py
@@ -0,0 +1,17 @@
+Q1.What is python.What are the ways to install python in your PC?
+
+Answer: 
+Python is a general purpose and high level programming language. You can use Python for developing desktop GUI applications, websites and web applications.Python can be used for things like: Back end (or server-side) web and mobile app development.
+
+*Steps to install paython in pc-
+Answer:
+
+Windows
+Step 1: Download the Python 3 Installer. Open a browser window and navigate to the Download page for Windows at python.org.
+
+Step 2: Run the Installer. Once you have chosen and downloaded an installer, simply run it by double-clicking on the downloaded file.
+
+Q2.what are code environments and what are its types?
+Answer:
+is a place where you can write your program, where you can compile or see your output of your program.
+Each code environment has its own set of packages. Environments are independent: you can install different packages or different versions of packages in different environments without interaction between them.Pycharm, VScCode, Sypder, Jyupter Notebook etc..are the various environments for Python
